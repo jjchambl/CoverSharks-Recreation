58589a2c79436488616c09dcdd3adf5c66580074
jjchambl <jjchambliss@gmail.com>
2016-04-11 22:25:00 -0500

    fonts configure

--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -1,7 +1,15 @@
-import {register, ui} from 'platypus';
+import {controls, events, register, ui, web} from 'platypus';
+import {DrawerController} from 'platypusui';
 
 export default class NavbarTemplateControl extends ui.TemplateControl {
     templateString: string = require('./navbar.tc.html');
+    // drawerController: controls.INamedElement<HTMLDivElement, DrawerController>;
+    
+    // initialize(): void {
+    //     this.on('navigated', (ev: events.DispatchEvent, utils: web.UrlUtils) => {
+    //     this.drawerController.control.close();
+    //     });
+    // }
 }
 
 register.control('navbar', NavbarTemplateControl);
