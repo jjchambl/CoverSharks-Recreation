1d64defd2efe6c3acd57e46906c95b0e812e5190
jjchambl <jjchambliss@gmail.com>
2016-04-14 13:06:16 -0500

    Home style finish

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -4,7 +4,18 @@ import BaseViewControl from '../base/base.vc';
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
-    context: any = {};
+    context: any = {
+        modal: false
+    };
+    
+    toggleModal() {
+        if(this.context.modal == false) {
+            this.context.modal = true;
+        }
+        else {
+            this.context.modal = false;
+        }
+    }
 }
 
 register.viewControl('home-vc', HomeViewControl);
