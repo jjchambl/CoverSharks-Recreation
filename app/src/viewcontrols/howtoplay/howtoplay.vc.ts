9d2487750ee51ecb0296b8f74047bc2f70ffe85d
banksnelsoe <banksnelson@mac.com>
2016-04-14 15:50:56 -0500

    Nav and Promo Changes

--- app/src/viewcontrols/howtoplay/howtoplay.vc.ts
+++ app/src/viewcontrols/howtoplay/howtoplay.vc.ts
@@ -1,10 +1,59 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
+// import VideosViewControl from '../viewcontrols/howtoplay/videos/videos.vc';
 
 export default class HowtoplayViewControl extends BaseViewControl {
     templateString: string = require('./howtoplay.vc.html');
 
-    context: any = {};
+    context: any = {
+        getStart: true,
+        getVideos: false
+    };
+    
+    initialize(): void {
+        console.log(`start: ${this.context.getStart}, videos: ${this.context.getVideos}`);
+    }
+    
+    startView() {
+        // if(this.context.getVideos === false) {
+        //     this.context.getStart = true;
+        // }
+        
+        // if (this.context.getVideos ===true) {
+        //     this.context.getStart = false;
+        // }
+        
+        // else {
+        //     this.context.get = true,
+        //     this.context.getStart = false;
+        // }
+        
+        this.context.getVideos = !this.context.getVideos,
+        this.context.getStart = !this.context.getStart;
+        
+        console.log(`start: ${this.context.getStart}, videos: ${this.context.getVideos}`);
+    }
+    
+    videoView() {
+        // if(this.context.getStart === false) {
+        //     this.context.getVideos = true;
+        // }
+        // if (this.context.getStart === true) {
+        //     this.context.getVideos = false;
+        // }
+        
+        // else {
+        //     this.context.getStart = false,
+        //     this.context.getVideos = true;
+        // }
+        
+        this.context.getStart = !this.context.getStart,
+        this.context.getVideos = !this.context.getVideos;
+        
+        
+        console.log(`start: ${this.context.getStart}, videos: ${this.context.getVideos}`);
+    }
 }
 
-register.viewControl('howtoplay-vc', HowtoplayViewControl);
+register.viewControl('howtoplay-vc', HowtoplayViewControl );
+
